42aa660028e56394a6b82746c51d65795df2e1cc
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-06-04 20:01:43 +1000

    flying meat

--- js/index.js
+++ js/index.js
@@ -1,68 +1,89 @@
-var x,
-  y;
-var angle1 = 0.0;
-var angle2 = 0.0;
-var legLength = 150;
-var armLength = 100;
+// We need 3 things everytime we use Three.js
+ // Scene + Camera + Renderer
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const renderer = new THREE.WebGLRenderer({ antialias: true})
 
-function setup() {
-  createCanvas(1000, 1000);
-  strokeWeight(30);
+renderer.setSize( window.innerWidth, window.innerHeight )
+// sets renderer background color
+renderer.setClearColor("#222222")
+document.body.appendChild( renderer.domElement )
+camera.position.z = 5
 
-  //Stroke with a semi-transparent white
-  stroke(255, 160);
-  fill(255, 160);
+// resize canvas on resize window
+window.addEventListener( 'resize', () => {
+	let width = window.innerWidth
+	let height = window.innerHeight
+	renderer.setSize( width, height )
+	camera.aspect = width / height
+	camera.updateProjectionMatrix()
+})
 
-  //Position the "shoulder" of the arm in the center of the canvas
+// basic cube
+var geometry = new THREE.BoxGeometry( 1, 1, 1)
+var material = new THREE.MeshStandardMaterial( { color: 0xff0051, flatShading: true, metalness: 0, roughness: 1 })
+var cube = new THREE.Mesh ( geometry, material )
+scene.add( cube )
 
+// wireframe cube
+var geometry = new THREE.BoxGeometry( 3, 3, 3)
+var material = new THREE.MeshBasicMaterial( {
+	color: "#dadada", wireframe: true, transparent: true
+})
+var wireframeCube = new THREE.Mesh ( geometry, material )
+scene.add( wireframeCube )
+
+// ambient light
+var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.2)
+scene.add( ambientLight )
+
+// point light
+var pointLight = new THREE.PointLight( 0xffffff, 1 );
+pointLight.position.set( 25, 50, 25 );
+scene.add( pointLight );
+
+
+// Instantiate a loader
+var loader = new THREE.GLTFLoader();
+
+
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'models/Waving.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Scene
+		gltf.scenes; // Array<THREE.Scene>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+function animate() {
+	requestAnimationFrame( animate )
+	cube.rotation.x += 0.04;
+	cube.rotation.y += 0.04;
+	wireframeCube.rotation.x -= 0.01;
+	wireframeCube.rotation.y -= 0.01;
+	renderer.render( scene, camera )
 }
-
-function draw() {
-  background(0);
-
-  //Change the angle of the segments according to the mouse positions
-  angle1 = (mouseX / float(width) - 0.5) * -TWO_PI;
-  angle2 = (mouseY / float(height) - 0.5) * PI;
-
-  //use push and pop to "contain" the transforms. Note that
-  // even though we draw the segments using a custom function,
-  // the transforms still accumulate
-
-  // LEFT ARM
-  push();
-  segment(120, 120, angle1, armLength);
-  segment(armLength, 0, angle2, armLength);
-  ellipse(armLength, 0, 20, 20)
-  pop();
-  // LEFT LEG
-  push();
-  segment(120, 300, angle1, legLength);
-  segment(legLength, 0, angle2, legLength);
-  ellipse(legLength, 0, 20, 20)
-  pop();
-  // RIGHT ARM
-  push();
-  segment(280, 120, angle1, armLength);
-  segment(armLength, 0, angle2, armLength);
-  ellipse(armLength, 0, 20, 20)
-  pop();
-  // RIGHT LEG
-  push();
-  segment(280, 300, angle1, legLength);
-  segment(legLength, 0, angle2, legLength);
-  ellipse(legLength, 0, 20, 20)
-  pop();
-  // BODY
-  push();
-  rect(100, 100, 200, 200, 40);
-  pop();
-}
-
-//a custom function for drawing segments
-function segment(x, y, a, length) {
-  translate(x, y);
-  rotate(a);
-  line(0, 0, length, 0);
-  stroke(255, 160);
-  fill(255, 160);
-}
+animate()
